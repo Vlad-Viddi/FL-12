efcbc15291e3d14594651cabf95fd9ed26f63ac5
Vladyslav Viddi <48797872+Vlad-Viddi@users.noreply.github.com>
2020-01-26 21:48:04 +0200

    upd1

--- FL12_HW11/homework/app.js
+++ FL12_HW11/homework/app.js
@@ -38,3 +38,24 @@ const structure = [
 const rootNode = document.getElementById('root');
 
 // Todo: your code goes here
+function toggleOpenClose() {
+  let pict = this.children[0].innerHTML;
+
+  if (pict === 'folder_open') {
+    this.children[0]innerHTML = 'folder'
+  } else {
+    this.children[0].innerHTML = 'folder_open'
+  }
+
+  let showFolder = this.nextElementSibling.style.display;
+
+  if (showFolder === 'visible') {
+    this.nextElementSibling.style.display = 'none';
+  } else {
+    this.nextElementSibling.style.display = 'block';
+  }
+}
+
+function draw_DOM_Tree (structure, parent) {
+  
+}
